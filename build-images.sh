d85265fccd5bb9d5c16cab5546b785f977fcc50b
Philip Falliano <philip.falliano@phillopp.de>
2024-06-27 00:08:45 +0200

    update service

--- build-images.sh
+++ build-images.sh
@@ -8,6 +8,8 @@
 # Terminate on error
 set -e
 
+GITLAB_VERSION=17.1.1
+
 # Prepare variables for later use
 images=()
 # The image will be pushed to GitHub container registry
@@ -36,10 +38,10 @@ buildah add "${container}" imageroot /imageroot
 buildah add "${container}" ui/dist /ui
 # Setup the entrypoint, ask to reserve one TCP port with the label and set a rootless container
 buildah config --entrypoint=/ \
-    --label="org.nethserver.authorizations=traefik@node:routeadm" \
+    --label="org.nethserver.authorizations=traefik@any:routeadm cluster:accountconsumer" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/jmalloc/echo-server:latest" \
+    --label="org.nethserver.images=docker.io/gitlab/gitlab-ce:${IMAGETAG}-ce.0" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
