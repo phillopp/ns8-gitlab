a2c860257239ca30b697034cf81d6dad8b4a32e6
Philip Falliano <philip.falliano@wohnbau-rm.de>
2024-07-13 15:04:00 +0200

    update config for registry

--- build-images.sh
+++ build-images.sh
@@ -38,7 +38,7 @@ buildah add "${container}" imageroot /imageroot
 buildah add "${container}" ui/dist /ui
 # Setup the entrypoint, ask to reserve one TCP port with the label and set a rootless container
 buildah config --entrypoint=/ \
-    --label="org.nethserver.authorizations=traefik@any:routeadm cluster:accountconsumer" \
+    --label="org.nethserver.authorizations=traefik@any:routeadm cluster:accountconsumer node:fwadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
     --label="org.nethserver.images=docker.io/gitlab/gitlab-ce:${GITLAB_VERSION}-ce.0" \
